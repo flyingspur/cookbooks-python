1e9385b7ff07ee478977aace0930f130d2bd5a72
flyingspur <seenu.ramachandran@gmail.com>
2019-01-12 21:01:16 +0530

    redoing to handle duplicate data

--- custom_hash_table/mydict.py
+++ custom_hash_table/mydict.py
@@ -1,73 +1,35 @@
 #!/usr/bin/env python
 
 class MyDict():
-"""A custom implementation of the dict object based on based
-on https://github.com/joeyajames/Python/blob/master/HashMap.py
-with my own logic for some of the operations
+"""A custom implementation of the dict object
 """
-
     def __init__(self, size):
         self.size = size
-        self.map = [None] * self.size
+        self.map = [None] * size
 
     def list(self):
         return self.map
 
-    def _get_hash(self, key):
-        hash = 0
-        for char in key:
-            hash += ord(char)
-        return hash % self.size
-
     def add(self, key, value):
-        key_hash = self._get_hash(key)
         key_value = [key, value]
 
-        if self.map[key_hash] is None:
-            self.map[key_hash] = list([key_value])
-            return True
-
-        exists = False
-        for kv in self.map[key_hash]:
-            if kv[0] == key:
-                exists = True
-        if exists:
-            self.map[key_hash].append(key_value)
-
-        return
-
-
-    def delete(self, key):
-        key_hash = self._get_hash(key)
-        temp_map = self.map[key_hash].copy()
-
-        for i,v in enumerate(temp_map):
-            if key == v[0]:
-                self.map[key_hash].pop(0)
-
-        if len(self.map[key_hash]) == 0:
-            self.map[key_hash] = None
-
-        return
-
-    def find(self, key):
-        key_hash = self._get_hash(key)
-        try:
-            for i,v in enumerate(self.map[key_hash]):
-                if key == v[0]:
-                    return self.map[key_hash]
-        except:
-            print('Key ' + key + ' does not exist.')
-
+        for i in range(0, self.size):
+            if self.map[i] is None:
+                self.map[i] = list([key_value])
+                return True
+            else:
+                k = self.map[i][0][0]
+                if k == key:
+                    self.map[i].append(key_value)
+                    return True
+                else:
+                    continue
         return False
 
-
-dict1 = MyDict(10)
-dict1.add('Pencils', 5)
-dict1.add('Pens', 10)
-dict1.add('Pens', 20)
-dict1.add('Erasers', 30)
-print("ADD: List of items: ", dict1.list())
-dict1.delete('Pencils')
-print("DELETE: List of items: ", dict1.list())
-print("FIND: Item: ", dict1.find('Pens'))
+    def delete(self, key):
+        for i in range(0, self.size):
+            k = self.map[i][0][0]
+            if k == key:
+                self.map.pop(i)
+                return True
+        return False
